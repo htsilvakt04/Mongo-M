36e57c3720ecca4554e00060cb991727a7e36cc4
silva <htsilvakt04@gmail.com>
2018-06-20 18:45:01 +0700

    env

--- bin/app.js
+++ bin/app.js
@@ -1,9 +1,10 @@
 const app = require('../server');
 const db = require('../db');
+const server_port = process.env.PORT || 3000;
 
 async function main () {
    const database =  await db();
-   await app(database).listen(3000, (err) => {
+   await app(database).listen(server_port, (err) => {
         if (err) console.log(err);
         console.log('app in running on port: 3000')
     });
